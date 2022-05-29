4c9fca3349eb50c32fc09c19ab4a95779f7f61e1
JonasHope <95291501+JonasHope@users.noreply.github.com>
2022-05-29 16:11:50 +0200

    design update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'apidojo-hm-hennes-mauritz-v1.p.rapidapi.com',
-		'X-RapidAPI-Key': 'b0f8ad1fa7msh77bf62184c7c0f8p177435jsn4aa90d59ecbb'
+function myFunction() {
+	const menu = document.querySelector("#nav-list");
+	
+	if (menu.style.display === "block") {
+	  menu.style.display = "none";
+	} else {
+	  menu.style.display = "block";
 	}
-};
-
-fetch('https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/products/list?country=asia2&lang=en&currentpage=0&pagesize=30&categories=men_all&sortBy=jackets&concepts=H%26M%20MAN', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+  }
