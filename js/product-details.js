34e3831cd71a77e397a6f31e4a6fccfb6c18b2c1
JonasHope <95291501+JonasHope@users.noreply.github.com>
2022-08-18 14:48:30 +0200

    test

---
+++ js/product-details.js
@@ -0,0 +1,78 @@
+const detailContainer = document.querySelector(".detail-container");
+const cart = document.querySelector(".cart")
+const cartList = document.querySelector(".cart-list")
+
+const queryString = document.location.search;
+
+const params = new URLSearchParams(queryString);
+
+const id = params.get("id");
+
+const url = "http://localhost/rD-products/wp-json/wc/store/products/" + id;
+
+async function getJackets() {
+
+    try{
+        const respons = await fetch(url);
+        const details = await respons.json();
+        
+        createHTML(details)
+    }
+    catch(error) {
+        console.log(error);
+        detailContainer.innerHTML = message("error", error);
+    }
+}
+
+getJackets();
+
+function createHTML(details) {
+    detailContainer.innerHTML = `<div class=jck-detail>
+                                     <h1>${details.name}</h1>
+                                     <h3>${details.price_html}</h3>
+                                     <img src="${details.images[0].src}" alt="${details.name}">
+                                     <p>${details.description}</p>
+                                     <button id="btn" class="cart-button">Add to cart</button>
+                                 </div>`;
+}
+
+let cartArray = [];
+
+const addToCartButtons = document.querySelectorAll("button");
+addToCartButtons.forEach(function(addToCartButton){
+  addToCartButton.onclick = function(event){
+    const itemToAdd = products.find(item => item.id === event.target.dataset.detailed)
+    cartArray.push(itemToAdd);
+    showCart(cartArray);
+    localStorage.setItem("cartList", JSON.stringify(cartArray));
+  }
+})
+
+const totalContainer = document.querySelector(".total")
+
+function showCart(cartItems){
+  cart.style.display = "block";
+  cartList.innerHTML = "";
+  let total = 0;
+  cartItems.forEach(function(cartElement){
+    total += parseFloat(cartElement.price);
+    cartList.innerHTML +=
+    `
+    <div class="cart-item">
+         <img src="${cartElement.image}" class="cart-img" alt="${cartElement.name}">
+         <h4>${cartElement.name}</h4>
+         <p class="price">£${cartElement.price}</p>
+         <hr>
+    </div>
+    `
+  })
+  totalContainer.innerHTML = `Total: £ ${total}`
+}
+
+const closeCart = document.querySelectorAll(".close-cart")
+
+for (const close of closeCart) {
+    close.onclick = function(){
+    cart.style.display = 'none'
+    }
+}
